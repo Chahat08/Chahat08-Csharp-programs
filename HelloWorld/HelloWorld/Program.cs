97f0ad3fb690901717498dfff7b753f119dbe82c
Chahat Kalsi <chahat.ck88@gmail.com>
2021-03-07 11:25:29 +0530

    Hello, World added

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
