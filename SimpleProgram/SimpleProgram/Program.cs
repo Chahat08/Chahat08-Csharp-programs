07f50c26647f776caa052b6dfc30a785095a29e0
Chahat Kalsi <chahat.ck88@gmail.com>
2021-03-07 18:49:58 +0530

    a simple program added

---
+++ SimpleProgram/SimpleProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProgram
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string name = "";
+            int birthYear = 0;
+            int currYear = 0;
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter the year you were born in: ");
+            birthYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the current year: ");
+            currYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Hello, {0}! You were born in {1} and are currently {2} years old.", name, birthYear, currYear - birthYear);
+
+            Console.Read();
+        }
+    }
+}
